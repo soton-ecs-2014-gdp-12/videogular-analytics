29e2935b642594f3643be78339057de2376f3c20
Christopher Baines <cb15g11@soton.ac.uk>
2014-10-26 09:46:25 +0000

    Begin to implement the plugin
    Listen to events in Videogular, and report them to the server.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-videogular-analytics
-====================
+[![Stories in Ready](https://badge.waffle.io/soton-ecs-2014-gdp-12/videogular-questions.png?label=ready&title=Ready)](https://waffle.io/soton-ecs-2014-gdp-12/videogular-questions)
+
+This repository is a plugin for the videogular video player that allows quizs and polls during the playback of videos.
+
+See [videogular questions example](https://github.com/soton-ecs-2014-gdp-12/videogular-questions-example) repository for examples to use with this repsitory.
---
+++ analytics.js
@@ -0,0 +1,70 @@
+(function(){
+"use strict";
+angular.module("uk.ac.soton.ecs.videogular.plugins.analytics", [])
+	.directive(
+		"vgAnalytics", ["VG_STATES", "$http", "$timeout",
+			function(VG_STATES, $http, $timeout) {
+				return {
+					restrict: "E",
+					require: "^videogular",
+					scope: {
+					},
+					link: function($scope, elem, attr, API) {
+
+						$scope.$parent.$on("analytics", function(event, name, details) {
+							sendEvent(name, details);
+						});
+
+						function onUpdateState(newState) {
+							switch (newState) {
+								case VG_STATES.PLAY:
+									sendEvent("play");
+									break;
+								case VG_STATES.STOP:
+									sendEvent("stop");
+									break;
+								case VG_STATES.PAUSE:
+									sendEvent("pause");
+									break;
+							}
+						}
+
+						function sendEvent(name, details) {
+							var now = new Date();
+							
+							var content = {
+								time: now.toISOString(),
+								name: name
+							};
+
+							if (typeof(details) !== "undefined") {
+								content.details = details;
+							}
+
+							$http.post('http://localhost:5000/log', content)
+								.success(function(data, status, headers, config) {
+								// this callback will be called asynchronously
+								// when the response is available
+							}).error(function(data, status, headers, config) {
+								console.error("error reporting event to the server");
+								// called asynchronously if an error occurs
+								// or server returns response with an error status.
+							});
+						}
+
+						$scope.$watch(
+							function () {
+								return API.currentState;
+							},
+							function (newVal, oldVal) {
+								if (newVal != oldVal) {
+									onUpdateState(newVal);
+								}
+							}
+						);
+					},
+				};
+			}
+		]
+	)
+})();
